3a9d9cd5b98334d5eeb613452a269d02e7124650
fazekasluca2007 <fazekasl@kkszki.hu>
2026-05-16 15:40:12 +0200

    Add project files.

---
+++ hegyekCLI/Hegycsucs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hegyekCLI
+{
+    public  class Hegycsucs
+    {
+        public string nev { get;  private set; }
+        public string hegyseg { get;  private set; }
+        public int magassag { get;  private set; }
+
+        public Hegycsucs(string nev, string hegyseg, int magassag)
+        {
+            this.nev = nev;
+            this.hegyseg = hegyseg;
+            this.magassag = magassag;
+        }
+        public Hegycsucs(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            this.nev = adatok[0];
+            this.hegyseg = adatok[1];
+            this.magassag = int.Parse(adatok[2]);
+        }
+
+
+    }
+}
---
+++ hegyekCLI/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hegyekCLI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var beolvasas = File.ReadAllLines("hegyek.csv").Skip(1);
+            List<Hegycsucs> hegyek = new List<Hegycsucs>();
+            foreach (var item in beolvasas)
+            {
+                hegyek.Add(new Hegycsucs(item));
+            }
+
+
+            //8.feladat
+            foreach (var item in hegyek)
+            {
+                if (item.magassag > 950)
+                {
+                    Console.WriteLine($"{item.nev} {item.hegyseg} {item.magassag}");
+                }
+            }
+            //11.feladat
+            Console.WriteLine("Kérem a keresett szót:");
+            string keresett=(Console.ReadLine());
+            Console.WriteLine("........");
+            foreach (var item in hegyek)
+            {
+                if (tartalmaz(item.nev, item.hegyseg, keresett))
+                {
+                    Console.WriteLine($"{item.nev}");
+                }
+            }
+        }
+
+        
+        public static bool tartalmaz(string nev, string hegyseg, string keresett)
+        {
+            if (hegyseg.Contains(keresett) || nev.Contains(keresett))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }  
+}
